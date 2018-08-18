3505d9597c886219c68b13d618d8f03d6f6eb7dc
Jason Lee <zzuljs@163.COM>
2018-08-18 15:30:49 +0800

    update code

--- CppLearning/Source.cpp
+++ CppLearning/Source.cpp
@@ -2,7 +2,37 @@
 
 using namespace std;
 
-int main() {
+int sort(int*a, int low, int high) {
+	int key = a[low];
+	while (low < high) {
+		while (low < high&&key <= a[high]) {
+			high--;
+		}
+		a[low] = a[high];
+		while (low < high&&key >= a[low]) {
+			low++;
+		}
+		a[high] = a[low];
+	}
+	a[low] = key;
+	return low;
+}
+void quick_sort(int*a, int low, int high) {
+	if (low >= high) {
+		return;
+	}
+	int part = sort(a, low, high);
+	quick_sort(a, low, part - 1);
+	quick_sort(a, part + 1, high);
+}
 
+int main() {
+	int a[] = { 12,45,78,97,65,46,456,147,135,648,31,1,22 };
+	int length = sizeof(a) / sizeof(int);
+	quick_sort(a, 0, length - 1);
+	for (int i = 0; i < length; i++) {
+		cout << a[i] << " ";
+	}
+	cin.get();
 	return 0;
 }
