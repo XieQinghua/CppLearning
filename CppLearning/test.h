67bcad3f70a9089ec4482707d60bf457921b1e8b
Jason Lee <zzuljs@163.COM>
2018-08-28 15:11:00 +0800

    create test.cpp and test.h

---
+++ CppLearning/test.h
@@ -0,0 +1,16 @@
+#pragma once
+
+void test1();
+void test2();
+void test3();
+void test4();
+void test5();
+void test6();
+void test7();
+void test8();
+void test9();
+void test10();
+void test11();
+void test12();
+void test13();
+void test14();
