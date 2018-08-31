18982c00154b8f8221869c83e811ded69992004d
Jason Lee <zzuljs@163.COM>
2018-08-31 22:01:34 +0800

    updating code

--- CppLearning/Source.cpp
+++ CppLearning/Source.cpp
@@ -6,12 +6,27 @@
 #include<iostream>
 #include<string>
 #include<algorithm>
+#include"Util.h"
 
 using namespace std;
 
-
 int main() {
-	
+	union Data {
+		struct {
+			int x;
+			int y;
+		} s;
+		int x;
+		int y;
+	}d;
+	d.x = 1;
+	d.y = 2;
+	cout << d.x << endl << d.y << endl;
+	d.s.x = d.x*d.x;
+	d.s.y = d.y + d.y;
+	cout << d.s.x << endl << d.s.y << endl;
+	cout << d.x << endl << d.y << endl;
 	cin.get();
 	return 0;
 }
+
