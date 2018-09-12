0f6eb50f2c2c360e763c6b424856819d5c8f9dd9
Jason Lee <zzuljs@163.COM>
2018-09-12 22:53:32 +0800

    code updating

--- CppLearning/Source.cpp
+++ CppLearning/Source.cpp
@@ -10,42 +10,7 @@
 #include"test.h"
 
 using namespace std;
-//
-//int main(int agc,char** args) {
-//	test_scanf_format();
-//	/*for (int i = 0; i < agc; i++) {
-//		cout << args[i] << endl;
-//	}*/
-//	cin.get();
-//	return 0;
-//}
-//
-//#include<iostream>
-//
-//using namespace std;
-//int function2(int a[], int b, int e)
-//{
-//	if (e - b <= 1) return abs(a[b] - a[e]) >= 3 ? a[b] : a[e];
-//	int l = 0, r = 0;
-//	l = function2(a, b, b + (e - b) / 2);
-//	if (l % 2 == 0)
-//		r = function2(a, b + (e - b) / 2 + 1, e);
-//	else
-//		return l;
-//	if (l | r)
-//		return l | r;
-//	else
-//		return r;
-//}
-//
-//int main()
-//{
-//	int a[] = { 10,5,8,4,5,20,2,3 };
-//	cout << function2(a, 0, sizeof(a) / sizeof(1)) << endl;
-//	int b[] = { 3,5,8,4,8,30,2,3 };
-//	cout << function2(b, 0, sizeof(b) / sizeof(1)) << endl;
-//	return 0;
-//}
+
 
 //int main()
 //{
@@ -89,13 +54,11 @@ using namespace std;
 //	return 0;
 //}
 
-int main() {
+Util u;
 
-	char c;
-	int a, b;
-	cin >> a  >> b;
-	cout << "a = " << a << endl;
-	cout << "b = " << b << endl;
+int main() {
+	cout << "MAIN()" << endl;
+	
 	cin.get();
 	cin.get();
 
